456d7fae2a8893b86d36ca99a3c4f86f85c8b0b3
hoxi777 <jtho1015@gmail.com>
2019-11-22 18:03:48 +0900

    It is just changing of setting

--- typecode-frontend/src/Apollo/Clinet.ts
+++ typecode-frontend/src/Apollo/Clinet.ts
@@ -1,10 +1,8 @@
 import ApolloClient from "apollo-boost";
 import { InMemoryCache } from "apollo-cache-inmemory";
 
-const cache = new InMemoryCache();
-
 const client = new ApolloClient({
-  cache,
+  cache: new InMemoryCache(),
   uri: "https://localhost:4000/"
 });
 
