690c570726abac6e295f49a368c674e3b1d46467
hoxi777 <jtho1015@gmail.com>
2019-12-05 15:19:10 +0900

    LoginPresenter Constructor

--- typecode-frontend/src/pages/Login/LoginPresenter.tsx
+++ typecode-frontend/src/pages/Login/LoginPresenter.tsx
@@ -1,8 +1,47 @@
-import React from "react"
+import React from "react";
+import {
+  Container,
+  Header,
+  Logo,
+  LoginWrap,
+  LoginLabel,
+  SocialLogin,
+  FacebookLogin,
+  GoogleLogin,
+  NaverLogin,
+  EmailLoginWrap,
+  EmailLoginLabel,
+  UserNameForm,
+  PasswordForm,
+  LoginBtn,
+  ForgotEmailOrPassword
+} from "./LoginStyles";
 
 const LoginPresenter: React.FC = () => (
-    <>
-    </>
-)
+  <Container>
+    <Header>
+      <Logo>TypeCode</Logo>
+    </Header>
+    <LoginWrap>
+      <LoginLabel>Login</LoginLabel>
+      <SocialLogin>
+        <FacebookLogin>페이스북 로그인</FacebookLogin>
+        <GoogleLogin>구글 로그인</GoogleLogin>
+        <NaverLogin>네이버 로그인</NaverLogin>
+      </SocialLogin>
+
+      <EmailLoginWrap>
+        <EmailLoginLabel>e-mail login</EmailLoginLabel>
+        <UserNameForm />
+        <PasswordForm />
+        <LoginBtn>Log in</LoginBtn>
+      </EmailLoginWrap>
+
+      <ForgotEmailOrPassword>
+        Forgot email <span>[Forgot Password]</span>
+      </ForgotEmailOrPassword>
+    </LoginWrap>
+  </Container>
+);
 
 export default LoginPresenter;
--- typecode-frontend/src/pages/Login/LoginStyles.tsx
+++ typecode-frontend/src/pages/Login/LoginStyles.tsx
@@ -3,4 +3,36 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   flex: 1 1 auto;
-`
+`;
+
+export const Header = styled.div``;
+
+export const Logo = styled.a`
+  color: #ffffff;
+  font-size: 32px;
+  text-decoration: none;
+`;
+
+export const LoginWrap = styled.div``;
+
+export const LoginLabel = styled.div``;
+
+export const SocialLogin = styled.div``;
+
+export const FacebookLogin = styled.div``;
+
+export const GoogleLogin = styled.div``;
+
+export const NaverLogin = styled.div``;
+
+export const EmailLoginWrap = styled.div``;
+
+export const EmailLoginLabel = styled.div``;
+
+export const UserNameForm = styled.input``;
+
+export const PasswordForm = styled.input``;
+
+export const LoginBtn = styled.button``;
+
+export const ForgotEmailOrPassword = styled.div``;
