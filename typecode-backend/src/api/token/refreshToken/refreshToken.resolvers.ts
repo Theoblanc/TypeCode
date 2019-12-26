c59482c8bd868d16b984e87aa81bf3ddd6e4b4f0
hoxi777 <jtho1015@gmail.com>
2019-12-26 19:36:24 +0900

    Updating refreshoken

--- typecode-backend/src/api/token/refreshToken/refreshToken.resolvers.ts
+++ typecode-backend/src/api/token/refreshToken/refreshToken.resolvers.ts
@@ -2,7 +2,13 @@ import { Resolvers } from "src/types/resolvers";
 
 const resolvers: Resolvers = {
   Mutation: {
-    createRefreshoken: async (_, args, ctx): Promise<any> => {}
+    createRefreshoken: async (_, args, ctx): Promise<any> => {
+      const { id, accessToken } = args;
+
+      const userId = ctx.user.id;
+
+      if (accessToken.exp == )
+    }
   }
 };
 
