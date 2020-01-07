5fc8e52615af0165acdf749f195c60029328fb05
TheoBlanc <jtho1015@gmail.com>
2020-01-07 23:56:54 +0900

    Change from refhook to props

--- typecode-frontend/src/components/Button/HomeButton.tsx
+++ typecode-frontend/src/components/Button/HomeButton.tsx
@@ -1,109 +1,23 @@
-import React, { useRef } from "react";
+import React from "react";
 import styled from "styled-components";
 
 // Style
 
 const Container = styled.div`
-  width: 100%;
-  height: 80px;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  background-color: #ffffff;
-`;
-const HoverBarContainer = styled.div`
-  width: 4px;
-`;
-
-const HoverBar = styled.div`
-  width: 4px;
-  height: 0px;
-  border-bottom-right-radius: 10px;
-  border-top-right-radius: 10px;
-`;
-
-const MoveButtonContainer = styled.a`
-  padding-left: 16px;
-  padding-right: 12px;
-  padding-top: 16px;
-  padding-bottom: 16px;
-`;
-
-const MoveButton = styled.div`
-  width: 50px;
-  height: 50px;
-  background: linear-gradient(45deg, #ee7752, #e73c7e);
-  border-radius: 30px;
+  height: 51px;
   display: flex;
   align-items: center;
   justify-content: center;
+  border-bottom: 1px solid #ee7752;
+  width: 100%;
+  color: #ee7752;
+  font-weight: bold;
+  :hover {
+    background: linear-gradient(90deg, #ee7752, #e73c7e);
+    color: #ffffff;
+  }
 `;
-
-const MoveButtonText = styled.div``;
-
-const Icon = styled.img`
-  width: 40px;
-  height: 40px;
-`;
-
-// onMouseEnter={this.onMouseEnterHandler}
-// onMouseLeave={this.onMouseLeaveHandler}
-
-// render
 
 export const HomeButton = () => {
-  const hoverBarRef = useRef<HTMLInputElement>(null);
-  const hoverBtnRef = useRef<HTMLInputElement>(null);
-
-  const _onMouseEnter = () => {
-    if (
-      hoverBtnRef &&
-      hoverBtnRef.current &&
-      hoverBarRef &&
-      hoverBarRef.current
-    ) {
-      hoverBtnRef.current.style.background =
-        "linear-gradient(45deg, #ee7752, #e73c7e)";
-      hoverBtnRef.current.style.color = "black";
-      hoverBtnRef.current.style.borderRadius = "15px";
-      hoverBarRef.current.style.background =
-        "linear-gradient(45deg, #ee7752, #e73c7e)";
-      hoverBarRef.current.style.height = "20px";
-    }
-  };
-
-  const _onMouseLeave = () => {
-    if (
-      hoverBtnRef &&
-      hoverBtnRef.current &&
-      hoverBarRef &&
-      hoverBarRef.current
-    ) {
-      hoverBtnRef.current.style.backgroundColor =
-        "linear-gradient(45deg, #ee7752, #e73c7e)";
-      hoverBtnRef.current.style.color = "#ffffff";
-      hoverBtnRef.current.style.borderRadius = "30px";
-      hoverBarRef.current.style.backgroundColor =
-        "linear-gradient(45deg, #ee7752, #e73c7e)";
-      hoverBarRef.current.style.height = "0px";
-    }
-  };
-  return (
-    <Container>
-      <HoverBarContainer>
-        <HoverBar ref={hoverBarRef}></HoverBar>
-      </HoverBarContainer>
-      <MoveButtonContainer href="/home">
-        <MoveButton
-          ref={hoverBtnRef}
-          onMouseLeave={_onMouseLeave}
-          onMouseEnter={_onMouseEnter}
-        >
-          <MoveButtonText>
-            <Icon src="https://img.icons8.com/color/48/000000/discord-logo.png"></Icon>
-          </MoveButtonText>
-        </MoveButton>
-      </MoveButtonContainer>
-    </Container>
-  );
+  return <Container>홈</Container>;
 };
--- typecode-frontend/src/components/Button/ListButton.tsx
+++ typecode-frontend/src/components/Button/ListButton.tsx
@@ -1,66 +1,22 @@
-import React, { useRef } from "react";
+import React from "react";
 import styled from "styled-components";
 
 // Style
 
 const Container = styled.div`
-  width: 100%;
-  height: 80px;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  background-color: #ffffff;
-`;
-const HoverBarContainer = styled.div`
-  width: 4px;
-`;
-
-const HoverBar = styled.div`
-  width: 4px;
-  height: 0px;
-  border-bottom-right-radius: 10px;
-  border-top-right-radius: 10px;
-`;
-
-const MoveButtonContainer = styled.div`
-  padding-left: 16px;
-  padding-right: 12px;
-  padding-top: 16px;
-  padding-bottom: 16px;
-`;
-
-const MoveButton = styled.a`
-  width: 50px;
-  height: 50px;
-  background: linear-gradient(45deg, #ee7752, #e73c7e);
-  border-radius: 30px;
+  height: 51px;
   display: flex;
   align-items: center;
   justify-content: center;
-
-  text-decoration: none;
-  color: #ffffff;
-  a:visited {
-    color: black;
+  border-bottom: 1px solid #ee7752;
+  width: 100%;
+  color: #ee7752;
+  font-weight: bold;
+  :hover {
+    background: linear-gradient(90deg, #ee7752, #e73c7e);
+    color: #ffffff;
   }
 `;
-
-const MoveButtonText = styled.span``;
-
-// const OverMoiveButton = styled.div`
-//   background-color: #fddf;
-//   color: black;
-//   border-radius: 15px;
-//   width: 50px;
-//   height: 50px;
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-// `;
-
-// color: black;
-// border-radius: 15px;
-// render
 
 interface IProps {
   roomName: string;
@@ -69,58 +25,5 @@ interface IProps {
 }
 
 export const ListButton: React.FC<IProps> = ({ id, roomName, moverRoom }) => {
-  const hoverBarRef = useRef<HTMLInputElement>(null);
-  const hoverBtnRef = useRef<HTMLInputElement>(null);
-
-  const _onMouseEnter = () => {
-    if (
-      hoverBtnRef &&
-      hoverBtnRef.current &&
-      hoverBarRef &&
-      hoverBarRef.current
-    ) {
-      hoverBtnRef.current.style.background =
-        "linear-gradient(45deg, #ee7752, #e73c7e)";
-      hoverBtnRef.current.style.color = "black";
-      hoverBtnRef.current.style.borderRadius = "15px";
-      hoverBarRef.current.style.background =
-        "linear-gradient(45deg, #ee7752, #e73c7e)";
-      hoverBarRef.current.style.height = "20px";
-    }
-  };
-
-  const _onMouseLeave = () => {
-    if (
-      hoverBtnRef &&
-      hoverBtnRef.current &&
-      hoverBarRef &&
-      hoverBarRef.current
-    ) {
-      hoverBtnRef.current.style.backgroundColor =
-        "linear-gradient(45deg, #ee7752, #e73c7e)";
-      hoverBtnRef.current.style.color = "#ffffff";
-      hoverBtnRef.current.style.borderRadius = "30px";
-      hoverBarRef.current.style.backgroundColor =
-        "linear-gradient(45deg, #ee7752, #e73c7e)";
-      hoverBarRef.current.style.height = "0px";
-    }
-  };
-
-  return (
-    <Container>
-      <HoverBarContainer>
-        <HoverBar ref={hoverBarRef}></HoverBar>
-      </HoverBarContainer>
-      <MoveButtonContainer onClick={moverRoom}>
-        <MoveButton
-          href={`/room/${roomName}`}
-          ref={hoverBtnRef}
-          onMouseLeave={_onMouseLeave}
-          onMouseEnter={_onMouseEnter}
-        >
-          <MoveButtonText>{roomName}</MoveButtonText>
-        </MoveButton>
-      </MoveButtonContainer>
-    </Container>
-  );
+  return <Container>{roomName}</Container>;
 };
